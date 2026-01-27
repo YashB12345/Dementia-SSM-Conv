0cef94dd2fe833efd0f18e89d4e963961a2b1365
YashB12345 <165397150+YashB12345@users.noreply.github.com>
2026-01-26 19:51:11 -0500

    adni-complete-1-5t-2yr-2890samples-sagittal

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ def main():
                                    transforms.ToTensor(),
                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])}
 
-    DATA_SET_PATH = '/kaggle/input/ADNI_Complete_1.5T_2YR_2890samples_sagittal'
+    DATA_SET_PATH = '/kaggle/input/adni-complete-1-5t-2yr-2890samples-sagittal'
 
     train_dataset = datasets.ImageFolder(root=DATA_SET_PATH,
                                          transform=data_transform["train"])
