21841235cf3ae02a0de722cad105a0c4c3a55911
YashB12345 <165397150+YashB12345@users.noreply.github.com>
2026-01-27 21:15:13 -0500

    adni1-complete-3yr-1-5t-3244samples-axial

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ def main():
                                    transforms.ToTensor(),
                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])}
 
-    DATA_SET_PATH = '/kaggle/input/adni1-complete-3yr-1-5t-3244samples'
+    DATA_SET_PATH = '/kaggle/input/adni1-complete-3yr-1-5t-3244samples-axial'
 
     train_dataset = datasets.ImageFolder(root=DATA_SET_PATH,
                                          transform=data_transform["train"])
