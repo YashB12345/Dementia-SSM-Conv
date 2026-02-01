027e926d2e5ef030ea758d94bd5d1c6a9dadc27a
YashB12345 <165397150+YashB12345@users.noreply.github.com>
2026-02-01 10:28:53 -0500

    Taking a -25 slice adni1-3yr-1-5t-3244samples-sagittal-25

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ def main():
                                    transforms.ToTensor(),
                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])}
 
-    DATA_SET_PATH = '/kaggle/input/higher-loss-precision-with-3244samples-sagittal'
+    DATA_SET_PATH = '/kaggle/input/adni1-3yr-1-5t-3244samples-sagittal-25'
 
     orig_dataset = datasets.ImageFolder(root=DATA_SET_PATH,
                                          transform=data_transform["train"])
