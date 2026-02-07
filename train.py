60c1e4152dbde555d416480440c0ac9c4a033b72
YashB12345 <165397150+YashB12345@users.noreply.github.com>
2026-02-07 12:29:14 -0500

    Undo all grayscale changes

--- train.py
+++ train.py
@@ -17,8 +17,7 @@ def main():
     print("using {} device.".format(device))
 
     data_transform = {
-        "train": transforms.Compose([transforms.Grayscale(num_output_channels=1), # Convert to grayscale
-                                     transforms.Resize((224, 224)),
+        "train": transforms.Compose([transforms.Resize((224, 224)),
                                      transforms.ToTensor(),
                                      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),
          "flip": transforms.Compose([  transforms.RandomResizedCrop(224),
