d25772436132a1cd098de7b9a750ca84dcb4db77
YashB12345 <165397150+YashB12345@users.noreply.github.com>
2026-02-09 22:26:10 -0500

    printing bcos dataset dimension inconsisten

--- train.py
+++ train.py
@@ -82,7 +82,8 @@ def main():
     print(f"Train set size: {len(train_dataset)}")
     print(f"Validation set size: {len(val_dataset)}")
     print(f"Test set size: {len(test_dataset)}")
-
+    print(f"DataSetshape: {train_dataset.shape}")
+    
     #validate_dataset = datasets.ImageFolder(root=DATA_SET_PATH,transform=data_transform["val"])
 
     val_num = len(val_dataset)
