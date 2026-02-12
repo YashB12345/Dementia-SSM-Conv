78d50b53c835f2cc9ae8764c1323cdbb254774e5
YashB12345 <165397150+YashB12345@users.noreply.github.com>
2026-02-11 20:54:29 -0500

    /kaggle/input/datasets/yfinity/higher-loss-precision-with-3244samples-sagittal

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ def main():
                                    transforms.ToTensor(),
                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])}
 
-    DATA_SET_PATH = '/kaggle/input/higher-loss-precision-with-3244samples-sagittal'
+    DATA_SET_PATH = '/kaggle/input/datasets/yfinity/higher-loss-precision-with-3244samples-sagittal'
 
     orig_dataset = datasets.ImageFolder(root=DATA_SET_PATH,
                                          transform=data_transform["train"])
