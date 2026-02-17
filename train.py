dff4d927df6bf6e051b15df17e62b96f13d09643
YashB12345 <165397150+YashB12345@users.noreply.github.com>
2026-02-16 19:01:30 -0500

    normalize by ImageNet params. No random resize
    This has shown promising results on the Alzheimer's OASIS original dataset. Trying on the ADNI data

--- train.py
+++ train.py
@@ -19,14 +19,14 @@ def main():
     data_transform = {
         "train": transforms.Compose([transforms.Resize((224, 224)),
                                      transforms.ToTensor(),
-                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),
-         "flip": transforms.Compose([  transforms.RandomResizedCrop(224),
+                                     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]),
+         "flip": transforms.Compose([  transforms.Resize((224, 224)),
                                      transforms.RandomHorizontalFlip(p=1),
                                      transforms.ToTensor(),
-                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),
+                                     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]),
         "val": transforms.Compose([transforms.Resize((224, 224)),
                                    transforms.ToTensor(),
-                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])}
+                                   transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])}
 
     DATA_SET_PATH = '/kaggle/input/datasets/yfinity/higher-loss-precision-with-3244samples-sagittal'
 
