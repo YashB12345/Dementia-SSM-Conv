ceef2498274e3bcbdb9a611aa0f32a9fac1252b6
YashB12345 <165397150+YashB12345@users.noreply.github.com>
2026-02-28 20:39:27 -0500

    adni-2273-1-5t-1yr-sagittal-25-skull-stripped-3

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ def main():
                                    transforms.ToTensor(),
                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])}
 
-    DATA_SET_PATH = '/kaggle/input/datasets/yfinity/adni-1583-1-5t-1yr-sagittal-skull-stripped-3'
+    DATA_SET_PATH = '/kaggle/input/datasets/yfinity/adni-2273-1-5t-1yr-sagittal-25-skull-stripped-3'
 
     orig_dataset = datasets.ImageFolder(root=DATA_SET_PATH,
                                          transform=data_transform["train"])
