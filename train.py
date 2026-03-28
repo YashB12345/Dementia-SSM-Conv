4bb8f3bb4ea15ea3225f36c600f7c2344430f6aa
YashB12345 <165397150+YashB12345@users.noreply.github.com>
2026-03-28 13:17:53 -0400

    adni-1-5t-axial-5to5-antspynet-flirt-n4correct

--- train.py
+++ train.py
@@ -29,7 +29,7 @@ def main():
                                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])}
 
 
-    DATA_SET_PATH = '/kaggle/input/datasets/yfinity/adni-1571-1-5t-axial-antspynet-flirt-n4correct'
+    DATA_SET_PATH = '/kaggle/input/datasets/yfinity/adni-1-5t-axial-5to5-antspynet-flirt-n4correct'
 
     orig_dataset = datasets.ImageFolder(root=DATA_SET_PATH,
                                          transform=data_transform["train"])
