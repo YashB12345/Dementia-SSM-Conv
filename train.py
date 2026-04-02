3e03da966f38326e0034bb97b2bcfcf44479c346
YashB12345 <165397150+YashB12345@users.noreply.github.com>
2026-04-01 20:05:47 -0400

    Trying to use K-Fold cross validation

--- train.py
+++ train.py
@@ -12,6 +12,9 @@ from torchsummary import summary
 from MedMamba import VSSM
 from torch.utils.data import Dataset, DataLoader, random_split
 
+from sklearn.model_selection import StratifiedKFold
+import numpy as np
+
 def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("using {} device.".format(device))
@@ -71,89 +74,111 @@ def main():
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
     #val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
     test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+    
+    targets = []
+    for i in range(len(full_dataset)):
+        _, label = full_dataset[i]
+        targets.append(label)
+    
+    targets = np.array(targets)
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
     # Verify the sizes
-    print(f"Train set size: {len(train_dataset)}")
-    print(f"Validation set size: {len(val_dataset)}")
-    print(f"Test set size: {len(test_dataset)}")
+    #print(f"Train set size: {len(train_dataset)}")
+    #print(f"Validation set size: {len(val_dataset)}")
+    #print(f"Test set size: {len(test_dataset)}")
 
     #validate_dataset = datasets.ImageFolder(root=DATA_SET_PATH,transform=data_transform["val"])
 
-    val_num = len(val_dataset)
-    validate_loader = torch.utils.data.DataLoader(val_dataset,
-                                                  batch_size=batch_size, shuffle=False,
-                                                  num_workers=nw)
-    print("using {} images for training, {} images for validation with {} classes".format(len(train_dataset),
-                                                                           val_num, len(class_to_idx)))
+    #val_num = len(val_dataset)
+    #validate_loader = torch.utils.data.DataLoader(val_dataset,batch_size=batch_size, shuffle=False,num_workers=nw)
+    #print("using {} images for training, {} images for validation with {} classes".format(len(train_dataset),val_num, len(class_to_idx)))
 
-    model_name = "VSSM"
-    net = VSSM(num_classes=len(class_to_idx))
-    net.to(device)
-    loss_function = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(net.parameters(), lr=0.0001)
+    for fold, (train_idx, val_idx) in enumerate(skf.split(np.zeros(len(targets)), targets)):
+        print(f"\n===== Fold {fold+1} =====")
+    
+        train_subset = torch.utils.data.Subset(dataset, train_idx)
+        val_subset = torch.utils.data.Subset(dataset, val_idx)
+    
+        train_loader = DataLoader(train_subset, batch_size=..., shuffle=True)
+        val_loader = DataLoader(val_subset, batch_size=..., shuffle=False)
+    
+        model_name = "VSSM"
+        net = VSSM(num_classes=len(class_to_idx))
+        net.to(device)
+        loss_function = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(net.parameters(), lr=0.0001)
 
-    #summary(net, input_size=(1, 224, 224))
+        #summary(net, input_size=(1, 224, 224))
 
-    epochs = 10
-    best_acc = 0.0
-    save_path = './{}Net.pth'.format(model_name)
-    train_steps = len(train_loader)
-    for epoch in range(epochs):
-        # train
-        net.train()
-        running_loss = 0.0
-        train_bar = tqdm(train_loader, file=sys.stdout)
-        for step, data in enumerate(train_bar):
-            images, labels = data
-            optimizer.zero_grad()
-            outputs = net(images.to(device))
-            loss = loss_function(outputs, labels.to(device))
-            loss.backward()
-            optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-
-            train_bar.desc = "train epoch[{}/{}] loss:{:.7f}".format(epoch + 1,
-                                                                     epochs,
-                                                                     loss)
-
-        # validate
-        net.eval()
-        acc = 0.0  # accumulate accurate number / epoch
-        with torch.no_grad():
-            val_bar = tqdm(validate_loader, file=sys.stdout)
-            for val_data in val_bar:
-                val_images, val_labels = val_data
-                outputs = net(val_images.to(device))
-                predict_y = torch.max(outputs, dim=1)[1]
-                acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
-
-            # Find indices where prediction != ground truth
-        incorrect_indices = (predict_y != val_labels).nonzero(as_tuple=True)[0]
-
-        val_accurate = acc / val_num
-        print('[epoch %d] train_loss: %.7f  val_accuracy: %.7f' %
-              (epoch + 1, running_loss / train_steps, val_accurate))
-        
-        print(f"Total evaluated {len(images)} and failed {len(incorrect_indices)}")
-        
-        for idx in incorrect_indices:
-            for i, item in enumerate(misclassified_images):
+        epochs = 10
+        best_acc = 0.0
+        save_path = './{}Net.pth'.format(model_name)
+        train_steps = len(train_loader)
+        for epoch in range(epochs):
+            # train
+            net.train()
+            running_loss = 0.0
+            train_bar = tqdm(train_loader, file=sys.stdout)
+            for step, data in enumerate(train_bar):
+                images, labels = data
+                optimizer.zero_grad()
+                outputs = net(images.to(device))
+                loss = loss_function(outputs, labels.to(device))
+                loss.backward()
+                optimizer.step()
+    
+                # print statistics
+                running_loss += loss.item()
+    
+                train_bar.desc = "train epoch[{}/{}] loss:{:.7f}".format(epoch + 1,
+                                                                         epochs,
+                                                                         loss)
+    
+            # validate
+            net.eval()
+            acc = 0.0  # accumulate accurate number / epoch
+            with torch.no_grad():
+                val_bar = tqdm(validate_loader, file=sys.stdout)
+                for val_data in val_bar:
+                    val_images, val_labels = val_data
+                    outputs = net(val_images.to(device))
+                    predict_y = torch.max(outputs, dim=1)[1]
+                    acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
+    
+                # Find indices where prediction != ground truth
+            incorrect_indices = (predict_y != val_labels).nonzero(as_tuple=True)[0]
+    
+            val_accurate = acc / val_num
+            print('[epoch %d] train_loss: %.7f  val_accuracy: %.7f' %
+                  (epoch + 1, running_loss / train_steps, val_accurate))
             
-                # Store (image, predicted_label, actual_label)
-                img = images[idx].to(device) #To resolve Expected all tensors to be on the same device, but found at least two devices, cuda:0 and cpu!
-                misclassified_images.append({
-                    "img": img,
-                    "pred": class_names[preds[idx].item()],
-                    "actual": class_names[labels[idx].item()]
-                })
+            print(f"Total evaluated {len(images)} and failed {len(incorrect_indices)}")
+            
+            for idx in incorrect_indices:
+                for i, item in enumerate(misclassified_images):
+                
+                    # Store (image, predicted_label, actual_label)
+                    img = images[idx].to(device) #To resolve Expected all tensors to be on the same device, but found at least two devices, cuda:0 and cpu!
+                    misclassified_images.append({
+                        "img": img,
+                        "pred": class_names[preds[idx].item()],
+                        "actual": class_names[labels[idx].item()]
+                    })
+    
+            print(f"Pred: {item['pred']}\nActual: {item['actual']}", color='red')
 
-        print(f"Pred: {item['pred']}\nActual: {item['actual']}", color='red')
+            if val_accurate > best_acc:
+                best_acc = val_accurate
+                torch.save(net.state_dict(), save_path)
+            
+            fold_results = []
+            
+            # after each fold
+            fold_results.append(best_val_acc)
+            
+            print("Average CV Accuracy:", np.mean(fold_results))
 
-        if val_accurate > best_acc:
-            best_acc = val_accurate
-            torch.save(net.state_dict(), save_path)
 
     print('Finished Training')
 
