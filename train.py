ea7bf35caedcef2ce3925d836f1235b33571419a
Yash B <yashbanerjee06880@gmail.com>
2026-04-02 22:11:09 -0400

    Doing StratKFold to train part of the dataset and rotate

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ def main():
                                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])}
 
 
-    DATA_SET_PATH = '/kaggle/input/datasets/yfinity/adni-1-5t-axial-5to5-antspynet-flirt-n4correct'
+    DATA_SET_PATH = '/kaggle/input/datasets/yfinity/adni-1-5t-axial-2to2-antst-flirt-n4correct-3450'
 
     orig_dataset = datasets.ImageFolder(root=DATA_SET_PATH,
                                          transform=data_transform["train"])
