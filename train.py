a7575736a9f69e09e33dd01bc8a8b58a36970865
YashB12345 <165397150+YashB12345@users.noreply.github.com>
2026-04-19 20:49:46 -0400

    testing on raja kaggle

--- train.py
+++ train.py
@@ -55,7 +55,7 @@ def main():
                                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])}
 
 
-    DATA_SET_PATH = '/kaggle/input/datasets/yfinity/adni1-1yr-1-5t-1571sam-antspynet-coronal-central'
+    DATA_SET_PATH = '/kaggle/input/datasets/rajab23456/adni1-1yr-1-5t-1571sam-antspynet-coronal-central'
 
     orig_dataset = datasets.ImageFolder(root=DATA_SET_PATH,
                                          transform=data_transform["train"])
